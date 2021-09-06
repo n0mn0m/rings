3e6799b46de03d9c47ec21b1103e37813eddbe19
Alexander Hagerman <ahagerman@renci.org>
2021-09-06 14:43:14 -0400

    setup eslint and tooling
    Add canvas tooling for test, configure eslint settings.

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'warn',
         '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off'
     },
     settings: {
         'react': {
