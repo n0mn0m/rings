29acb7bc8050e320016efcd6e760c263ed1108e3
Alexander Hagerman <ahagerman@renci.org>
2021-09-06 14:44:00 -0400

    formatting

--- src/draw.ts
+++ src/draw.ts
@@ -1,17 +1,18 @@
-import Paper from "paper";
-import { Color } from "paper/dist/paper-core";
+import Paper from 'paper';
+import { Color } from 'paper/dist/paper-core';
 
-const draw = () : void => {
-  const path = new Paper.Path();
+const draw = (): void => {
+    Paper.project.activate();
+    const path = new Paper.Path();
 
-  Paper.view.onMouseDown = () => {
-    path.strokeColor = new Color('hsla(282, 80%, 63%, 1)');
-    path.strokeWidth = 3;
-  };
+    Paper.view.onMouseDown = () => {
+        path.strokeColor = new Color('hsla(282, 80%, 63%, 1)');
+        path.strokeWidth = 3;
+    };
 
-  Paper.view.onMouseDrag = (mouseEvent: paper.MouseEvent) => {
-    path.add(mouseEvent.point);
-  };
+    Paper.view.onMouseDrag = (mouseEvent: paper.MouseEvent) => {
+        path.add(mouseEvent.point);
+    };
 };
 
 export default draw;
